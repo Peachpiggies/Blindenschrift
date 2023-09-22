dc674929a27796c0ca97c330dcc1234e14f01abe
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-09-22 22:29:27 +0700

    added PIL for the image preview in the application.

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ import csv
 
 window = Tk()
 window.title('Blidenschrift')
-window.geometry('300x350')
+window.geometry('400x480')
 window.resizable(width = True, height = True)
 
 label1 = Label(window, text = 'Blidenschrift', font = ('Bahnschrift light', 20), fg = 'black').pack(pady = 15)
+
+frame = Frame(window, width=360, height=360)
+frame.pack()
+frame.place(anchor='center', relx=0.5, rely=0.5)
+
+img = ImageTk.PhotoImage(Image.open("img_asset/blindenschrift.jpg"))
+
+label = Label(frame, image = img)
+label.pack()
 
 window.mainloop()
