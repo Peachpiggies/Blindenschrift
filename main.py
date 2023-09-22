d1897c8678a92148bc22bdd2b923fa2527cd5e7d
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-09-22 21:54:06 +0700

    First I used PySimpleGUI, but now I using tkinter for the gui.

---
+++ main.py
@@ -0,0 +1,11 @@
+import braille as b
+import cv2
+import numpy as np
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Blidenschrift')
+window.geometry('300x350')
+window.resizable(width = True, height = True)
+
+window.mainloop()
