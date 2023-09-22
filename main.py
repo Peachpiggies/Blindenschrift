6aafa699e267a37b614dd8f55cfa4edcd4c4792b
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-09-22 22:19:38 +0700

    added the application name in the middle

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import braille as b
 import cv2
 import numpy as np
-import tkinter as tk
+from tkinter import *
+from tkinter.messagebox import askokcancel
+from PIL import ImageTk, Image
+import csv
 
-window = tk.Tk()
+window = Tk()
 window.title('Blidenschrift')
 window.geometry('300x350')
 window.resizable(width = True, height = True)
+
+label1 = Label(window, text = 'Blidenschrift', font = ('Bahnschrift light', 20), fg = 'black').pack(pady = 15)
 
 window.mainloop()
