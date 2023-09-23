77e8f9caaf29d14e98cc2094ac089cce52ea08fd
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-09-23 13:18:33 +0700

    added a image button by using a lable and a button

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import csv
 
 window = Tk()
 window.title('Blidenschrift')
-window.geometry('500x600')
+window.geometry('640x1136')
 window.resizable(width = True, height = True)
 
 # Created a base font for the whole application
@@ -20,7 +20,7 @@ label1 = Label(window, text = 'Blidenschrift', font = BL36, fg = 'black').pack(p
 
 frame = Frame(window, width=360, height=360)
 frame.pack()
-frame.place(anchor='center', relx=0.5, rely=0.5)
+frame.place(anchor='center', relx = 0.5, rely = 0.45)
 
 img = ImageTk.PhotoImage(Image.open("img_asset/blindenschrift.jpg"))
 
@@ -30,6 +30,12 @@ label.pack()
 # Created a button to scan the
 scan_b = Button(window, width = 5, height = 1, text = "Scan", font = BL20, fg = '#bcb531', bg = '#3476ae')
 scan_b.pack()
-scan_b.place(anchor = 'center', relx = 0.5, rely = 0.9)
+scan_b.place(anchor = 'center', relx = 0.5, rely = 0.8)
+
+img_b = ImageTk.PhotoImage(file = 'img_asset/setting.jpg')
+img_lable = Label(window, image = img_b)
+setting_b = Button(window, image = img_b)
+setting_b.pack()
+setting_b.place(relx = 0.85, rely = 0.85)
 
 window.mainloop()
