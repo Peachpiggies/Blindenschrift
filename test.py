c324ee9834e2b46b713bd092ca3fcabe4e82585c
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-09-28 18:07:20 +0700

    better but still get unsatisfied output

--- test.py
+++ test.py
@@ -1,44 +1,37 @@
 import cv2
 import numpy as np
-import pytesseract
-import os
 
-os.environ['TESSDATA_PREFIX'] = 'C:/Users/Peachpiggies/AppData/Local/Programs/Tesseract-OCR'
-
-# Define a dictionary to map Braille characters to English characters
-braille_to_english = {
-
-        '⠁': 'a',
-        '⠃': 'b',
-        '⠉': 'c', 
-        '⠙': 'd',
-        '⠑': 'e', 
-        '⠋': 'f',
-        '⠛': 'g', 
-        '⠓': 'h',
-        '⠊': 'i', 
-        '⠚': 'j',
-        '⠅': 'k',
-        '⠇': 'l',
-        '⠍': 'm',
-        '⠝': 'n',
-        '⠕': 'o',
-        '⠏': 'p',
-        '⠟': 'q',
-        '⠗': 'r',
-        '⠎': 's',
-        '⠞': 't',
-        '⠥': 'u',
-        '⠧': 'v',
-        '⠺': 'w',
-        '⠭': 'x',
-        '⠽': 'y',
-        '⠵': 'z'
-
-    }
+# Define Braille patterns and their corresponding English characters
+braille_patterns = {
+    '⠁': 'a',
+    '⠃': 'b',
+    '⠉': 'c',
+    '⠙': 'd',
+    '⠑': 'e',
+    '⠋': 'f',
+    '⠛': 'g',
+    '⠓': 'h',
+    '⠊': 'i',
+    '⠚': 'j',
+    '⠅': 'k',
+    '⠇': 'l',
+    '⠍': 'm',
+    '⠝': 'n',
+    '⠕': 'o',
+    '⠏': 'p',
+    '⠟': 'q',
+    '⠗': 'r',
+    '⠎': 's',
+    '⠞': 't',
+    '⠥': 'u',
+    '⠧': 'v',
+    '⠺': 'w',
+    '⠭': 'x',
+    '⠽': 'y',
+    '⠵': 'z',
+}
 
 def detect_braille(image_path):
-    
     # Load the image using OpenCV
     image = cv2.imread(image_path)
     if image is None:
@@ -48,7 +41,6 @@ def detect_braille(image_path):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Perform image processing to detect Braille dots (you may need to fine-tune this)
-    # This is a basic example using thresholding
     _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
     # Find contours in the thresholded image
@@ -78,6 +70,7 @@ def detect_braille(image_path):
     return braille_characters
 
 def translate_braille_image_to_text(braille_char_image):
+
     # Check if the input image is None or empty
     if braille_char_image is None or len(braille_char_image) == 0:
         return ""
@@ -85,19 +78,45 @@ def translate_braille_image_to_text(braille_char_image):
     # Convert the Braille character image to grayscale
     gray = cv2.cvtColor(braille_char_image, cv2.COLOR_BGR2GRAY)
 
-    # Use pytesseract to recognize text from the image
-    custom_config = r'--psm 6 -l braille'  # Set language to Braille
-    braille_text = pytesseract.image_to_string(gray, config=custom_config)
+    # Check if the grayscale image is empty
+    if gray is None:
+        return ""
 
-    return braille_text
+    # Define the Braille cell positions for each dot
+    dot_positions = [
+        (1, 1), (1, 2),
+        (2, 1), (2, 2),
+        (3, 1), (3, 2)
+    ]
+
+    # Initialize an empty Braille pattern
+    braille_pattern = ""
+
+    # Convert the Braille character image to grayscale
+    gray = cv2.cvtColor(braille_char_image, cv2.COLOR_BGR2GRAY)
+
+    # Iterate through the dot positions
+    for position in dot_positions:
+        x, y = position
+        # Check if the dot is present (black pixel)
+        if gray[y, x] == 0:
+            # Append the dot position to the Braille pattern
+            braille_pattern += '1'
+        else:
+            # Append a space for no dot
+            braille_pattern += '0'
+
+    # Map the Braille pattern to an English character (or '?' for unknown patterns)
+    english_char = braille_patterns.get(braille_pattern, '?')
+
+    return english_char
 
 def translate_braille_to_english(braille_characters):
     english_text = ""
     for braille_char_image in braille_characters:
-        # You need to implement a function to translate the braille image to text
+        # Translate each Braille character and append to the result
         braille_char_text = translate_braille_image_to_text(braille_char_image)
-        english_char = braille_to_english.get(braille_char_text, "?")  # Default to "?" for unknown characters
-        english_text += english_char
+        english_text += braille_char_text
 
     return english_text
 
