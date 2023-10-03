29d96e59c6ab17680476681bfc832c14a65f1ac8
Natthanan Rungjarudnon <97142113+Peachpiggies@users.noreply.github.com>
2023-10-03 11:51:44 +0700

    edit some line

--- MyBraille/TestYolo.py
+++ MyBraille/TestYolo.py
@@ -1,10 +1,12 @@
 import torch
 from ultralytics import YOLO
+import braille
 import braille2
 
 def something(img_path):
     model = YOLO("yolov8_braille.pt")
     result = braille2.BrailleToText(str(model.predict(img_path)))
-    print(result)
+    converted_result = braille.brailleToTextArray(result)
+    print(converted_result)
 
 something('./test01.jpg')
